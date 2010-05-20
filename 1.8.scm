d9adb992b03bd0021ed026628ada5ed36c9895f5
Steve Tooke <steve.tooke@gmail.com>
2010-05-20 07:13:40 +0100

    Refactor to general root-iterator

--- 1.8.scm
+++ 1.8.scm
@@ -1,31 +1,47 @@
 (load "test-manager/load.scm")
 
-(define (cube-root-iter guess last-guess x)
+(define (root-iter improve guess last-guess x)
   (if (good-enough? guess last-guess x)
     guess
-    (cube-root-iter (improve guess x)
-                    guess
-                    x)))
+    (root-iter improve
+               (improve guess x)
+               guess
+               x)))
 
-(define (improve guess x)
+(define (cube-improve guess x)
   (/
     (+ (/ x (square guess))
        (* 2 guess))
     3))
 
+(define (sqrt-improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
 (define (good-enough? guess last-guess x)
-  (< (abs (- guess last-guess)) (/ guess 100)))
+  (< (abs (- guess last-guess)) (/ guess 100000)))
 
 (define (cube-root x)
-  (cube-root-iter 1.0 1.1 x))
+  (root-iter cube-improve 1.0 1.1 x))
+
+(define (sqrt x)
+  (root-iter sqrt-improve 1.0 1.1 x))
 
   (in-test-group
     change-in-guess-approximation
 
-    (define-test (very-simple)
+    (define-test (very-simple-cube)
       (check (= (cube-root 8) 2)))
 
-    (define-test (simple)
-      (check (= (cube-root 64) 4))))
+    (define-test (very-simple-square)
+      (check (= (sqrt 4) 2)))
+
+    (define-test (simple-cube)
+      (check (= (cube-root 64) 4)))
+
+    (define-test (simple-square)
+      (check (= (sqrt 16) 4))))
 
   (run-registered-tests)
